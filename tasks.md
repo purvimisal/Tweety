7054d1cdde670b8deae4947a740a13c90145e56f
purvimisal <purvivmisal@gmail.com>
2020-01-13 14:49:12 -0800

    Added models, views(rest)

---
+++ tasks.md
@@ -0,0 +1,27 @@
+1. Users
+    - Register
+    - Login
+    - Logout
+    - Profile
+        - Image
+        - Text 
+        - Follow Button
+    - Feed
+        - User's feed
+        - User + who they follow
+
+2. Tweets
+    - Creating
+        - Text
+        - Image
+    - Delete
+    - Retweeting
+
+3. Following/ Follower
+
+
+
+Extensive ToDos:
+- Notifications
+- DM
+- Explore Tab / searching on hashtags
---
+++ tweets/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from .models import Tweet
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW 
+    for it to be able to be consumed by JavaScript, etc
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id":x.id, "content": x.content} for x in qs]
+    data ={
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW 
+    for it to be able to be consumed by JavaScript, etc
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
