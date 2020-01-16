3b61a572a957779ecb85d97853310587755629ce
purvimisal <purvivmisal@gmail.com>
2020-01-15 23:10:30 -0800

    adding react components for tweets views

--- twitterprototype-web/src/index.js
+++ twitterprototype-web/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {TweetsComponent} from './tweets'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appEl = document.getElementById('root')
+
+if (appEl) {
+  ReactDOM.render(<App />, appEl);
+}
+const tweetsEl = document.getElementById("twittwit")
+if (tweetsEl) {
+  ReactDOM.render(<TweetsComponent />, tweetsEl);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- twitterprototype/urls.py
+++ twitterprototype/urls.py
@@ -9,10 +9,15 @@ from tweets.views import (
     tweet_delete_view,
     tweet_action_view,
     )
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
+    path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
     path('create-tweet', tweet_create_view),
     path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
@@ -20,3 +25,8 @@ urlpatterns = [
     # path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
     path('api/tweets/', include('tweets.urls'))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, 
+        document_root=settings.STATIC_ROOT)
